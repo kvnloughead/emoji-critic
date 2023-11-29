8de0f4a5d22d7774b15f416a781c113b8dc4590d
Kevin Loughead <kvnloughead@gmail.com>
2023-11-29 14:37:25 -0500

    feat: add Dashboard component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1>Emoji Critic — All About Emojis</h1>
+      <p>
+        The #1 Destination for Emoji Reviews on the World Wide Web Since 2020!
+      </p>
+    </div>
+  );
+}
+
+export default Dashboard;

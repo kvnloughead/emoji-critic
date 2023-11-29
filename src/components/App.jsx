bb65eca526191ee781251be7cfb20a0a24be413f
Kevin Loughead <kvnloughead@gmail.com>
2023-11-29 14:39:24 -0500

    feat: add root route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import "./App.css";
+import { Route } from "react-router-dom";
+
 import Header from "./Header";
+import Dashboard from "./Dashboard";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Route path="/">
+        <Dashboard />
+      </Route>
     </div>
   );
 }
